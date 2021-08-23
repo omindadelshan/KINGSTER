f333edfa1cf4069aede30ee9ece683a9cb886ff1
Ominda <85678266+omindadelshan@users.noreply.github.com>
2021-08-23 19:46:20 +0530

    Add files via upload

---
+++ AnkiVector/modules/exam.py
@@ -0,0 +1,19 @@
+_help = " " "  
+ 🎉Now our Rose bot can tell you the exact result of your exam.
+
+  ❤️So special thanks to @uvindubro for doing this job.
+
+Here are the commands 🔰👇
+ To Get 
+
+ • (A/L)  Results - /al Index No
+ 
+ • (O/L) Results - /ol Index No
+ 
+ • G5 SCHOLARSHIP Results - /g5 Index No
+
+ If you have not yet added to the group, add it now 🤖
+@
+
+" " " 
+_mod_name = "Resalt🎲"
