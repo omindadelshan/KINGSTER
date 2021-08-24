a8d4d4bf8271b36378a658f772d2cd1aa3388fe5
Ominda <85678266+omindadelshan@users.noreply.github.com>
2021-08-24 16:57:12 +0530

    Add files via upload

---
+++ AnkiVector/modules/vc.py
@@ -0,0 +1,32 @@
+_help ="""
+
+× 👉First Add Me To Your Group..👈
+× 👉Promote Me As Admin In Your Group With All Permission..👈
+× 👉And Send a group /userbotjoin Command👈
+
+🏷 Common Commands.
+
+• /play - Song Name : Plays Via Youtube🎈
+• /dplay - Song Name : Play Via Deezer🎈
+• /splay - Song Name : Play Via Jio Saavn🎈
+• /playlist - Show now playing list🎈
+• /current - Show now playing🎈
+
+• /song - Song Name : Get The Song From YouTube🎈
+• /vid - Video Name : Get The Video From YouTube🎈
+• /deezer - song name : download songs you want quickly via deezer🎈
+• /saavn - song name : download songs you want quickly via saavn🎈
+• /search - YouTube Title : (Get YouTube Search Query)🎈
+
+🏷 Group Admin Commands.
+
+• /skip : Skips Music👨‍💻
+• /pause : Pause Playing Music👨‍💻
+• /resume : Resume Playing Music👨‍💻
+• /end : Stops playing Music👨‍💻
+• /reload : Reloads Admin List👨‍💻
+• /userbotjoin : Assistant Joins The Group👨‍💻
+• /userbotleave : Assistant Leaves From The Group👨‍💻.
+👉 Powerd By @sdprojectupdates 👈
+""" 
+_mod_name = "VC Music🎙️"
