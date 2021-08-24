99f2bb2eb80764ca4b9fba8186dfba27e9918c00
Ominda <85678266+omindadelshan@users.noreply.github.com>
2021-08-24 12:41:25 +0530

    Update misc.py

--- AnkiVector/modules/misc.py
+++ AnkiVector/modules/misc.py
@@ -655,7 +655,7 @@ __help__ = """
 \
 """
 
-__mod_name__ = "🎈Miscs🎈"
+__mod_name__ = "Miscs🎈"
 
 APP_HANDLER = DisableAbleCommandHandler("app", app)
 LYRICS_HANDLER = DisableAbleCommandHandler("lyrics", lyrics, pass_args=True)
