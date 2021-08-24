bf835a967f4a0f18e49f1e00d3ed577d2d904de9
Ominda <85678266+omindadelshan@users.noreply.github.com>
2021-08-24 12:18:53 +0530

    Update rules.py

--- AnkiVector/modules/rules.py
+++ AnkiVector/modules/rules.py
@@ -126,7 +126,7 @@ __help__ = """
  ✪ /clearrules*:* clear the rules for this chat.
 """
 
-__mod_name__ = "📜 Rules 📜"
+__mod_name__ = "Rules 📜"
 
 GET_RULES_HANDLER = CommandHandler("rules", get_rules, filters=Filters.group)
 SET_RULES_HANDLER = CommandHandler("setrules", set_rules, filters=Filters.group)
