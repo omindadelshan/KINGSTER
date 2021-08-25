d65edd66df8ab660c9b7588c902a1b4b276c0c48
Ominda <85678266+omindadelshan@users.noreply.github.com>
2021-08-25 12:05:13 +0530

    Update nightmode.py

--- AnkiVector/modules/nightmode.py
+++ AnkiVector/modules/nightmode.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2021 Sadew Jayasekara
 
 
-# This file is part of Emma (Telegram Bot)
+# This file is part of AnkiVector (Telegram Bot)
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
