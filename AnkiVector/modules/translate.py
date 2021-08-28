c449e5e445cc77a8c33799a454b5b67c28d24e2a
Ominda <85678266+omindadelshan@users.noreply.github.com>
2021-08-28 19:53:47 +0530

    Create translate.py

---
+++ AnkiVector/modules/translate.py
@@ -0,0 +1,6 @@
+__help__ = """
+Kingster Can Translate Langueges🧧
+✪ /tr - send A This Fromat👇
+   /tr (imput languege code)-(output languege code) Type Her..And Send"""
+
+__mod_name__ = "G-Translate🎁"
