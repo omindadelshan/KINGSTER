c3571da494d4027eec1ff49d36f8117162ee46f3
Ominda <85678266+omindadelshan@users.noreply.github.com>
2021-08-29 17:27:05 +0530

    Update and rename vc.py to gtranslate.py

---
+++ AnkiVector/modules/gtranslate.py
@@ -0,0 +1,5 @@
+__help__ = """
+Kingster Can Translaye Langueges😯
+✪ /tr (impput languege code)-(output languege code) enter Yor words and send..👉af,am,ar,az,be,bg,bn,bs,ca,ceb,co,cs,cy,da,de,el,en,eo,es,et,eu,fa,fi,fr,fy,ga,gd,gl,gu,ha,haw,hi,hmn,hr,ht,hu,hy,id,ig,is,it,iw,ja,jw,ka,kk,km,kn,ko,ku,ky,la,lb,lo,lt,lv,mg,mi,mk,ml,mn,mr,ms,mt,my,ne,nl,no,ny,pa,pl,ps,pt,ro,ru,sd,si,sk,sl,sm,sn,so,sq,sr,st,su,sv,sw,ta,te,tg,th,tl,tr,uk,ur,uz,vi,xh,yi,yo,zh,zh_CN,zh_TW,zu👈 My languege Codes🥴
+"""
+__mod_name__ = "Translate🎁"
--- AnkiVector/modules/vc.py
+++
@@ -1,5 +0,0 @@
-__help__ = """
-Kingster Can Translaye Langueges😯
-✪ /tr (impput languege code)-(output languege code) enter Yor words and send..
-"""
-__mod_name__ = "Translate🎁"
